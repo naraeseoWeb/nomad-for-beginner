cd11f0448ac35459474286d380e8a9578b7ff6ec
naraeseo <naraeseoez@gmail.com>
2022-09-29 14:16:08 +0900

    7.1: To Do List Part Two Complete

--- src/App.js
+++ src/App.js
@@ -1,41 +1,32 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [toDo, setToDo] = useState('');
-  const [toDos, setToDos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
 
-  const onChange = (e) => {
-    setToDo(e.target.value);
-  };
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (toDo.trim() === '') {
-      return;
-    }
-    setToDo('');
-    setToDos((currentArray) => [toDo, ...currentArray]);
-  };
-  console.log(toDos);
+  useEffect(() => {
+    fetch('https://api.coinpaprika.com/v1/tickers')
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type='text'
-          placeholder='Write your to do'
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? '' : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin, index) => (
+            <option key={index}>
+              {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price.toFixed(2)}
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
