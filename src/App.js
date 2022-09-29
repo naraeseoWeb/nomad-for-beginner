ad3ceb8e723ff11be5f94fb49deb2588da0dd54b
naraeseo <naraeseoez@gmail.com>
2022-09-29 13:49:30 +0900

    7.0: To Do List part One

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import { useState } from 'react';
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const onClick = () => setValue((prev) => prev + 1);
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
+
+  const onChange = (e) => {
+    setToDo(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (toDo.trim() === '') {
+      return;
+    }
+    setToDo('');
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  };
+  console.log(toDos);
+
   return (
     <div>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type='text'
+          placeholder='Write your to do'
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
