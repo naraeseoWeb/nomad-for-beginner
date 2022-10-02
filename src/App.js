2f285161ebf385691a3e59cca6ad6a0412f74f7d
naraeseo <naraeseoez@gmail.com>
2022-10-02 23:14:40 +0900

    switch 버전 해결 필요

--- src/App.js
+++ src/App.js
@@ -1,42 +1,16 @@
-import { useEffect, useState } from 'react';
-import Movie from './Movie';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './routes/Home';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `http://kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=f5eef3421c602c6cb7ea224104795888`
-      )
-    ).json();
-    setMovies(json.movieListResult.movieList);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  console.log('데이터', movies);
-
   return (
-    <div>
-      {loading ? (
-        <h2>Loading...</h2>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <Movie
-              title={movie.movieNm}
-              genre={movie.genreAlt}
-              nation={movie.nationAlt}
-              year={movie.prdtYear}
-            />
-          ))}
-        </div>
-      )}
-    </div>
+    <Router>
+      {/* <Switch> */}
+      <Route path='/'>
+        <Home />
+      </Route>
+      {/* </Switch> */}
+    </Router>
   );
 }
 
---
+++ src/routes/Home.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import Movie from '../components/Movie';
+
+const Home = () => {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `http://kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=f5eef3421c602c6cb7ea224104795888`
+      )
+    ).json();
+    setMovies(json.movieListResult.movieList);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  console.log('데이터', movies);
+
+  return (
+    <div>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <Movie
+              key={movie.movieCd}
+              title={movie.movieNm}
+              genre={movie.genreAlt}
+              nation={movie.nationAlt}
+              year={movie.prdtYear}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
